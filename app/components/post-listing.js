ca9334e7a87ff8a0e4bc8652452b83448414df99
andrew dang & Vaughn Dice <student@epicodus.com>
2015-09-23 14:28:53 -0700

    add post route and single-post component

--- app/components/post-listing.js
+++ app/components/post-listing.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   sortedPostsProps: ['date:desc'],
   sortedPosts: Ember.computed.sort('posts', 'sortedPostsProps'),
+
   actions: {
     update(post, params) {
       this.sendAction('update', post, params);
